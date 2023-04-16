1c0379a96699370fa9f60295c6f52e0b410f6ade
RaySanTech <RaySanTech@gmail.com>
2023-04-16 12:27:56 -0400

    Practice Commit

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,21 @@
 import random
-help = ("What is your name? ","What is your birthday? ","Help ")
+import time
+current_time = time.localtime()
+formatted_time = time.strftime("%H:%M:%S", current_time)
+
+
+help = ("What is your name? ","What is your birthday? ","What time is it?","Help")
 while True:
     c=input("Hello. Let's have a chat. If you need to see the list of commands, please type 'Help' ")
-    if c==("Help").lower:
-        print(help[0,2])
+    if c==("help"):
+        print(help)
     elif c==("What is your name?"):
         print("I don't have a name, but you can just call me chatbot for now. ")
     elif c==("What is your birthday?"):
         print("I was created on 4/16/2023 by 'RS' ")
-else:
-    print("I do not understand")
-
-    
+    elif c==("What time is it?"):
+        print("The current time is:", formatted_time)
+    else:
+        print("I do not understand")
+   
     
